15477ceb3505e1d6518bc8b5d05261a76563b21b
2017.12.10 <2273844523@qq.com>
2026-02-10 23:57:27 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,29 @@
+export const config = {
+  runtime: 'edge',
+};
 
+export default async function handler(request) {
+  const { searchParams } = new URL(request.url);
+  const targetUrl = searchParams.get('url');
+
+  if (!targetUrl) {
+    return new Response('Missing "url" parameter', { status: 400 });
+  }
+
+  const response = await fetch(targetUrl, {
+    headers: request.headers,
+    method: request.method,
+    body: request.body,
+  });
+
+  const newHeaders = new Headers(response.headers);
+  newHeaders.set('Access-Control-Allow-Origin', '*');
+  newHeaders.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  newHeaders.set('Access-Control-Allow-Headers', '*');
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: newHeaders,
+  });
+}
